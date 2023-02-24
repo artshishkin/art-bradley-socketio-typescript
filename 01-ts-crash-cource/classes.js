921c823103dcc1aa8bd8e1e0aa1a32163095175a
Art <d.art.shishkin@gmail.com>
2023-02-24 21:32:26 +0200

    6. Classes

---
+++ 01-ts-crash-cource/classes.js
@@ -0,0 +1,16 @@
+var Grault = /** @class */ (function () {
+    function Grault(quux, waldo) {
+        this.garply = quux.quuz + " " + quux.corge + " " + waldo;
+    }
+    Grault.prototype.getGarply = function () {
+        return this.garply;
+    };
+    return Grault;
+}());
+var qBar = {
+    corge: 1,
+    quuz: "ABC"
+};
+var someN = [1, 2, 3];
+var grault = new Grault(qBar, someN);
+console.log(grault.getGarply());
---
+++ 01-ts-crash-cource/classes.ts
@@ -0,0 +1,27 @@
+class Grault {
+    private garply: string;
+
+    constructor(quux: Quux, waldo: number[]) {
+        this.garply = quux.quuz + " " + quux.corge + " " + waldo;
+    }
+
+    public getGarply(): string {
+        return this.garply;
+    }
+}
+
+interface Quux {
+    quuz: string
+    corge: number
+}
+
+let qBar: Quux = {
+    corge: 1,
+    quuz: "ABC"
+}
+
+let someN = [1, 2, 3]
+
+let grault = new Grault(qBar, someN)
+
+console.log(grault.getGarply())
