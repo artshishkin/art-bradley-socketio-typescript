78ff5114184f34fe7199eb4fdbceeaa989323779
Art <d.art.shishkin@gmail.com>
2023-02-24 20:57:14 +0200

    4. Type Annotations

--- 01-ts-crash-cource/foo.js
+++ 01-ts-crash-cource/foo.js
@@ -1,5 +1,12 @@
 function foo(bar) {
     return "Hello, " + bar;
 }
+function fooUnion(bar) {
+    return "Hello, " + bar;
+}
 var baz = "ABC";
+var transport = "car";
+console.log(foo());
 console.log(foo(baz));
+console.log(fooUnion(transport));
+console.log(fooUnion(1));
--- 01-ts-crash-cource/foo.ts
+++ 01-ts-crash-cource/foo.ts
@@ -1,7 +1,15 @@
-function foo(bar) {
+function foo(bar?: string) {
+    return "Hello, " + bar;
+}
+
+function fooUnion(bar: "car" | "bus" | number) {
     return "Hello, " + bar;
 }
 
 let baz = "ABC";
+let transport: "car" | "bus" = "car";
 
+console.log(foo());
 console.log(foo(baz));
+console.log(fooUnion(transport));
+console.log(fooUnion(1));
