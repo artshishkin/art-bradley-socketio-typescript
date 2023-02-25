d9a3818b2d4712d1b3b83f7bce9d0db682e16e2b
Art <d.art.shishkin@gmail.com>
2023-02-25 17:49:57 +0200

    15.3 Server Socket Emit - rollback to simple string message

--- dist/client/client.js
+++ dist/client/client.js
@@ -2,9 +2,9 @@
 class Client {
     constructor() {
         this.socket = io(); //io("http://127.0.0.1:3000")
-        this.socket.on("message", (user) => {
-            console.dir(user);
-            document.body.innerHTML = JSON.stringify(user);
+        this.socket.on("message", (mess) => {
+            console.dir(mess);
+            document.body.innerHTML = JSON.stringify(mess);
         });
     }
 }
--- src/client/client.ts
+++ src/client/client.ts
@@ -1,16 +1,11 @@
-type User = {
-    name: string
-    age: number
-}
-
 class Client {
     private socket: SocketIOClient.Socket
 
     constructor() {
         this.socket = io() //io("http://127.0.0.1:3000")
-        this.socket.on("message", (user: User) => {
-            console.dir(user)
-            document.body.innerHTML = JSON.stringify(user);
+        this.socket.on("message", (mess: any) => {
+            console.dir(mess)
+            document.body.innerHTML = JSON.stringify(mess);
         })
     }
 }
