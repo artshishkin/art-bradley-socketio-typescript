b62b77da4b1ab7b1a80520e603b4830d35b736c8
Art <d.art.shishkin@gmail.com>
2023-02-26 12:28:31 +0200

    19. Client Connect and Disconnect Events

--- dist/client/client.js
+++ dist/client/client.js
@@ -2,6 +2,16 @@
 class Client {
     constructor() {
         this.socket = io(); //io("http://127.0.0.1:3000")
+        this.socket.on('connect', function () {
+            console.log('connect');
+            document.body.innerHTML = '';
+        });
+        this.socket.on('disconnect', function (message) {
+            console.log('disconnect ' + message);
+            document.body.innerHTML +=
+                `Disconnected from Server : ${message} <br/>`;
+            //location.reload();
+        });
         this.socket.on("message", (mess) => {
             console.dir(mess);
             document.body.innerHTML += mess + '<br/>';
--- src/client/client.ts
+++ src/client/client.ts
@@ -3,6 +3,19 @@ class Client {
 
     constructor() {
         this.socket = io() //io("http://127.0.0.1:3000")
+
+        this.socket.on('connect', function () {
+            console.log('connect')
+            document.body.innerHTML = ''
+        })
+
+        this.socket.on('disconnect', function (message: any) {
+            console.log('disconnect ' + message)
+            document.body.innerHTML +=
+                `Disconnected from Server : ${message} <br/>`
+            //location.reload();
+        })
+
         this.socket.on("message", (mess: any) => {
             console.dir(mess)
             document.body.innerHTML += mess + '<br/>';
